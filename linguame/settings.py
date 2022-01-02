e0571d844bade4f4cc0a43a81ecc7a0e5b974f74
kanghua <kanghua58@gmail.com>
2022-01-02 08:36:54 +0800

    securiity

--- linguame/settings.py
+++ linguame/settings.py
@@ -16,11 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yco$if2t^=8(!dk0#apa_b2o^fiir*jw61ry)e3qytqb1#7r$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
